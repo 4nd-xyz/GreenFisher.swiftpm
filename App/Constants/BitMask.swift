72e43136f8af7880fcfa9718360ed8a7c3dc5bf1
anderson <77030548+4nd-xyz@users.noreply.github.com>
2022-04-02 17:41:00 -0300

    Add: Constant for bitMask control

---
+++ App/Constants/BitMask.swift
@@ -0,0 +1,16 @@
+//
+//  BitMask.swift
+//
+//
+//  Created by Anderson on 07/04/22.
+//
+
+import Foundation
+
+enum BitMask {
+  static let tree     : UInt32 = 0b00001
+  static let watter   : UInt32 = 0b00010
+  static let node     : UInt32 = 0b00011
+  static let fisherMan: UInt32 = 0b01100
+  static let player   : UInt32 = 0b01111
+}
