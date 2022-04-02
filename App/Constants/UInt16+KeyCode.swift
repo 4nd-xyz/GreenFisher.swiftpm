bb1ac1f6842a0e20a520058affa4dd9adc0dd48e
anderson <77030548+4nd-xyz@users.noreply.github.com>
2022-04-02 17:41:00 -0300

    Add: Constants for gamepad control

---
+++ App/Constants/UInt16+KeyCode.swift
@@ -0,0 +1,16 @@
+//
+//  UInt16+KeyCode.swift
+//
+//
+//  Created by Anderson on 09/04/22.
+//
+
+import Foundation
+
+extension UInt16 {
+  static let space       : UInt16 = 0x31
+  static let leftArrow   : UInt16 = 0x7B
+  static let rightArrow  : UInt16 = 0x7C
+  static let downArrow   : UInt16 = 0x7D
+  static let upArrow     : UInt16 = 0x7E
+}
