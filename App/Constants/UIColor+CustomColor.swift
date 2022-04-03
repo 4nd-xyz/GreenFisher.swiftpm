d434c40139c4509faf3129ad8d5749129245bb7d
anderson <77030548+4nd-xyz@users.noreply.github.com>
2022-04-03 17:41:00 -0300

    Add: Color constants

---
+++ App/Constants/UIColor+CustomColor.swift
@@ -0,0 +1,16 @@
+//
+//  UIColor.swift
+//  
+//
+//  Created by Anderson on 09/04/22.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+  static let _0x0 = UIColor(named: "0x0-green")
+  static let _0x1 = UIColor(named: "0x1-green")
+  static let _0x2 = UIColor(named: "0x2-green")
+  static let _0x3 = UIColor(named: "0x3-green")
+}
