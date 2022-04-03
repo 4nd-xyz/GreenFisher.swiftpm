9b8434b6128efe9fb1a23e6445da57b7256a8713
anderson <77030548+4nd-xyz@users.noreply.github.com>
2022-04-03 17:41:00 -0300

    Add: Extension for custom font create

---
+++ App/Extension/CustomFont.swift
@@ -0,0 +1,20 @@
+//
+//  CustomFont.swift
+//  
+//
+//  Created by Anderson on 07/04/22.
+//
+
+import SpriteKit
+
+extension UIFont {
+
+  static func makeFont() -> String {
+    let pathFont = "EarlyGameBoy"
+    let fontURL = Bundle.main.url(forResource: pathFont, withExtension: "ttf")
+    guard let fontURL = fontURL else { fatalError("Deu ruim na url") }
+    CTFontManagerRegisterFontsForURL(fontURL as CFURL, CTFontManagerScope.process, nil)
+    return "Early GameBoy"
+  }
+  
+}
